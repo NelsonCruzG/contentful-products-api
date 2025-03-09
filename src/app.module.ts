f73439af0aafcf1e6057e3e47f875fd4544bf491
Nelson Cruz <nelsoncruz.1250@gmail.com>
2025-03-09 17:23:57 -0600

    ➕ feat: database connection
    - add: typeorm configuration
    - add: env validation schema
    - add: other security/validation dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { environmentSchema } from './config/environment.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseOptions } from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: environmentSchema,
+    }),
+    TypeOrmModule.forRoot(databaseOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/environment.config.ts
@@ -0,0 +1,44 @@
+import * as Joi from 'joi';
+
+export const environmentSchema = Joi.object({
+  CONTENTFUL_SPACE_ID: Joi.string().required().messages({
+    'string.required':
+      'CONTENTFUL_SPACE_ID env variable is required but not set',
+  }),
+  CONTENTFUL_ACCESS_TOKEN: Joi.string().required().messages({
+    'string.required':
+      'CONTENTFUL_ACCESS_TOKEN env variable is required but not set',
+  }),
+  CONTENTFUL_ENVIRONMENT: Joi.string().required().messages({
+    'string.required':
+      'CONTENTFUL_ENVIRONMENT env variable is required but not set',
+  }),
+  CONTENTFUL_CONTENT_TYPE: Joi.string().required().messages({
+    'string.required':
+      'CONTENTFUL_CONTENT_TYPE env variable is required but not set',
+  }),
+  POSTGRES_DB_HOST: Joi.string().required().messages({
+    'string.required': 'POSTGRES_DB_HOST env variable is required but not set',
+  }),
+  POSTGRES_DB_PORT: Joi.number().required().messages({
+    'number.base': 'POSTGRES_DB_PORT env var must be number',
+    'number.port': 'POSTGRES_DB_PORT env var must be a valid port',
+    'number.required': 'POSTGRES_DB_PORT env var is required but not set',
+  }),
+  POSTGRES_DB_USERNAME: Joi.string().required().messages({
+    'string.required':
+      'POSTGRES_DB_USERNAME env variable is required but not set',
+  }),
+  POSTGRES_DB_PASSWORD: Joi.string().required().messages({
+    'string.required':
+      'POSTGRES_DB_PASSWORD env variable is required but not set',
+  }),
+  POSTGRES_DB_NAME: Joi.string().required().messages({
+    'string.required': 'POSTGRES_DB_NAME env variable is required but not set',
+  }),
+  APP_PORT: Joi.number().port().default(3000).messages({
+    'number.base': 'APP_PORT env var must be number',
+    'number.port': 'APP_PORT env var must be a valid port',
+    'number.required': 'APP_PORT env var is required but not set',
+  }),
+});
