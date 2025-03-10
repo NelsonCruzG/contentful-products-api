f22e63cdc16bc366c23f4a315622fd5b48373e9e
Nelson Cruz <nelsoncruz.1250@gmail.com>
2025-03-09 22:04:07 -0600

    ➕ feat: add swagger for documentation
    - add: swagger dependency
    - add: docker compose file for prod
    - update: README file format

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('Contentful Products API')
+    .setDescription('Products from Contentful API via syncronization strategy.')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.APP_PORT ?? 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
